3b965f36969142e4f8020d4622e222acacdebc88
LKWLaLa <lkwlala@gmail.com>
2016-05-14 19:00:15 -0400

    routes.rb nested steps and videos inside of user urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
+
+   root 'home#index'
+
   
-  resources :videos
-  resources :steps
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :videos, only: [:index]
+  resources :steps, only: [:index]
+ 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" },
+  :paths => 'users' 
 
-  # You can have the root of your site routed with "root"
-  root 'home#index'
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users, only: [] do
+    resources :videos
+    resources :steps
+  end
+    
+  
 
 
 
