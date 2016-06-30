662c56ad0dcd64bb4882ab7076573ccd70238a54
LKWLaLa <lkwlala@gmail.com>
2016-06-29 20:45:25 -0400

    add videos.js file, add handlebars template (video show page html) to bottom of videos index

---
+++ app/assets/javascripts/videos.js
@@ -0,0 +1,7 @@
+function Video(attributes){
+  this.id = attributes.id;
+  this.title = attributes.title;
+  this.notes = attributes.notes;
+  this.url = attributes.url;
+  this.user_id = attributes.user_id;  
+} 
