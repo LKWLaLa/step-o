b9bffddcd55e7c5fa81849e0e8b9ed8f2304ec25
LKWLaLa <lkwlala@gmail.com>
2016-06-30 21:03:26 -0400

    store original panel html in data attribute, to retrieve in the Close listener event.  Solved problem of returning li/panel to its original state.

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,4 +1,4 @@
-var indexContent = '';
+
 var user = '';
 
 
@@ -36,8 +36,9 @@ function videoOpenListener(){
 
           $('li div.panel').each(function(index, body){
             if ($(this).find('.panel-body').data().id === videoId){
-              indexContent = $(this).find('.panel-body').html();
+              var indexHTML = $(this).find('.panel-body').html();
               $(this).find('.panel-body').html(replacementHTML);
+              $(this).data( 'old_html', indexHTML );
               $(this).find('.trigger').attr("class","revert pull-right").text("Close");
               $(this).find('iframe').attr("src", "http://www.youtube.com/embed/" + YouTubeGetID(videoObject.url));
             };
@@ -53,7 +54,7 @@ function videoCloseListener(){
      videoId = $(event.target).data();
     $('li div.panel').each(function(index, body){
       if ($(this).find('.panel-body').data().id === videoId.id){
-        $(this).find('.panel-body').html(indexContent);
+        $(this).find('.panel-body').html($(this).data().old_html);
         $(this).find('.revert').attr("class","trigger pull-right").text("Show");
        };;
     });
