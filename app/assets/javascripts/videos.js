f42dc66bbdd4fc3109c8a518d7f1c95972d4a701
LKWLaLa <lkwlala@gmail.com>
2016-07-14 23:08:49 -0400

    refactor videos.js to assign handlebars template as a Video object property

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -2,6 +2,14 @@
 var user = '';
 
 
+$(function(){  
+  user = $('.container#videos_index').data().userId;
+  addTemplateToVideo();
+  videoOpenListener();
+  videoCloseListener();
+ });
+
+
 function Video(attributes){
   this.id = attributes.id;
   this.title = attributes.title;
@@ -12,19 +20,21 @@ function Video(attributes){
   this.timemarkers = attributes.timemarkers;  
 } 
 
-$(function(){
-  Video.templateSource = $("#video-template").html();
-  Video.template = Handlebars.compile(Video.templateSource);
-  user = $('.container#videos_index').data().userId;
-  videoOpenListener();
-  videoCloseListener();
-});
+  function addTemplateToVideo(){
+    Video.templateSource = $("#video-template").html();
+    Video.template = Handlebars.compile(Video.templateSource);
+  }
+
+
+  Video.prototype.renderDropdown = function(){
+    return Video.template(this);
+  }
 
 
 
-function videoOpenListener(){
-  $('li').on('click', '.trigger', function(event){
-    videoId = $(event.target).data().id;
+  function videoOpenListener(){
+    $('li').on('click', '.trigger', function(event){
+    var videoId = $(event.target).data().id;
 
     $.ajax({
       url: 'http://localhost:3000/users/' + user + '/videos/' + videoId,
@@ -33,14 +43,15 @@ function videoOpenListener(){
 
      }).done(function(data){
           var videoObject = new Video(data);
-          var replacementHTML = Video.template(videoObject);
 
           $('li div.panel').each(function(index, body){
-            if ($(this).find('.panel-body').data().id === videoId){
+            if ($(this).find('.panel-body').data().id === videoId){              
               var indexHTML = $(this).find('.panel-body').html();
+
               $(this).find('.panel-body').hide();
-              $(this).find('.panel-body').html(replacementHTML);
+              $(this).find('.panel-body').html(videoObject.renderDropdown());
               $(this).find('.panel-body').slideDown();
+
               $(this).data( 'old_html', indexHTML );
               $(this).find('.trigger').attr("class","revert pull-right btn btn-primary").text("Close");
               $(this).find('iframe').attr("src", "http://www.youtube.com/embed/" + YouTubeGetID(videoObject.url));
@@ -52,33 +63,34 @@ function videoOpenListener(){
  
 
 
-function videoCloseListener(){
-  $('li').on('click', '.revert', function(event){
-     videoId = $(event.target).data();
-    $('li div.panel').each(function(index, body){
-      if ($(this).find('.panel-body').data().id === videoId.id){
-        $(this).find('.panel-body').html($(this).data().old_html);
-        $(this).find('.revert').attr("class","trigger pull-right btn btn-primary").text("Show");
-       };;
+  function videoCloseListener(){
+    $('li').on('click', '.revert', function(event){
+       var videoId = $(event.target).data().id;
+
+      $('li div.panel').each(function(index, body){      
+        if ($(this).find('.panel-body').data().id === videoId){
+          $(this).find('.panel-body').html($(this).data().old_html);
+          $(this).find('.revert').attr("class","trigger pull-right btn btn-primary").text("Show");
+         };;
+      });
     });
-  });
-}
+  }
 
 
 // YouTubeGetID function found at:  https://gist.github.com/takien/4077195
 
-function YouTubeGetID(url){
-  var ID = '';
-  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-  if(url[2] !== undefined) {
-    ID = url[2].split(/[^0-9a-z_\-]/i);
-    ID = ID[0];
+  function YouTubeGetID(url){
+    var ID = '';
+    url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    if(url[2] !== undefined) {
+      ID = url[2].split(/[^0-9a-z_\-]/i);
+      ID = ID[0];
+    }
+    else {
+      ID = url;
+    }
+      return ID;
   }
-  else {
-    ID = url;
-  }
-    return ID;
-}
 
 
 
