c10a2215a726d57f9f896331ff9b7af9bf9c9a10
LKWLaLa <lkwlala@gmail.com>
2016-09-22 15:36:00 -0400

    add first_name and last_name fields for user, and change greeting from email to first name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920202949) do
+ActiveRecord::Schema.define(version: 20160922191943) do
 
   create_table "step_styles", force: :cascade do |t|
     t.integer  "step_id"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20160920202949) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "first_name",             default: ""
+    t.string   "last_name",              default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
