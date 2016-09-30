9fcfcae7b8d98c48aa223f7040a08b95835d4781
LKWLaLa <lkwlala@gmail.com>
2016-09-30 16:58:14 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160922191943) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "step_styles", force: :cascade do |t|
     t.integer  "step_id"
     t.integer  "style_id"
@@ -63,10 +66,10 @@ ActiveRecord::Schema.define(version: 20160922191943) do
     t.string   "last_name",              default: ""
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["provider"], name: "index_users_on_provider"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["uid"], name: "index_users_on_uid"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
   create_table "videos", force: :cascade do |t|
     t.string   "url"
